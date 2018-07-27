7d95a597d753956d1061e0490d3a7aa9b4cab0e9
Brock <brockfanning@gmail.com>
2018-07-27 22:32:55 +0000

    Automated deployment to GitHub Pages: 0a220c25b5f51ba3b2514be4b83216f9c94bd6b0

--- assets/js/mapView.js
+++ assets/js/mapView.js
@@ -1,12 +1,15 @@
 var mapView = function () {
-  
+
   "use strict";
-  
+
   this.initialise = function(geoData, geoCodeRegEx) {
     $('.map').show();
     $('#map').sdgMap({
       geoData: geoData,
-      geoCodeRegEx: geoCodeRegEx
+      geoCodeRegEx: geoCodeRegEx,
+      serviceUrl: '/sdg-indicators-1/assets/js/gz_2010_us_040_00_500k.json',
+      nameProperty: 'NAME',
+      idProperty: 'GEO_ID'
     });
   }
 };
