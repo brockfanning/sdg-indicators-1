9a0b87006c26efc9199846ca1b4464ef71f9fc9c
Brock Fanning <brockfanning@gmail.com>
2018-09-24 17:25:29 -0400

    Trying a prod-pages deployment for production.

--- scripts/deploy/circleci/deploy_prod.sh
+++ scripts/deploy/circleci/deploy_prod.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
+# This script deploys the built site to the prod-pages branch of the same repo.
 git config --global user.email $GH_EMAIL
 git config --global user.name $GH_NAME
 
 # CircleCI will identify the SSH key with a "Host" of gh-stg. In order to tell
 # Git to use this key, we need to hack the SSH key:
-sed -i -e 's/Host gh-prod/Host gh-prod\n  HostName github.com/g' ~/.ssh/config
-# Now we can use "gh-prod" below to identify the SSH key to use.
-git clone git@gh-prod:$GH_ORG_PROD/$GH_ORG_PROD.github.io.git out
+sed -i -e 's/Host gh-stg/Host gh-stg\n  HostName github.com/g' ~/.ssh/config
+# Now we can use "gh-stg" below to identify the SSH key to use.
+git clone git@gh-stg:$GH_ORG_STG/$CIRCLE_PROJECT_REPONAME.git out
 
 cd out
-git checkout master || git checkout --orphan master
+git checkout prod-pages || git checkout --orphan prod-pages
 git rm -rfq .
 cd ..
 
@@ -20,6 +21,6 @@ mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
 cd out
 
 git add -A
-git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+git commit -m "Automated deployment to prod-pages: ${CIRCLE_SHA1}" --allow-empty
 
-git push origin master
+git push origin prod-pages
--- scripts/deploy/circleci/deploy_staging.sh
+++ scripts/deploy/circleci/deploy_staging.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# This script deploys the built site to the gh-pages branch of the same repo.
 git config --global user.email $GH_EMAIL
 git config --global user.name $GH_NAME
 
@@ -6,7 +7,7 @@ git config --global user.name $GH_NAME
 # Git to use this key, we need to hack the SSH key:
 sed -i -e 's/Host gh-stg/Host gh-stg\n  HostName github.com/g' ~/.ssh/config
 # Now we can use "gh-stg" below to identify the SSH key to use.
-git clone git@gh-stg:$GH_ORG_STG/sdg-indicators-1.git out
+git clone git@gh-stg:$GH_ORG_STG/$CIRCLE_PROJECT_REPONAME.git out
 
 cd out
 git checkout gh-pages || git checkout --orphan gh-pages
