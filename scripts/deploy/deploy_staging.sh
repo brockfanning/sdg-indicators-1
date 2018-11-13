a920da490991c13f5c5eda0321386f41e6713131
sustainabledevelopment-germany <44999984+sustainabledevelopment-germany@users.noreply.github.com>
2018-11-13 14:26:49 +0100

    Update deploy_staging.sh

--- scripts/deploy/deploy_staging.sh
+++ scripts/deploy/deploy_staging.sh
@@ -24,8 +24,9 @@ echo "TRAVIS_TAG = " $TRAVIS_TAG
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 
-chmod 600 ./scripts/deploy/deploy_key_ds
 eval `ssh-agent -s`
+chmod 600 ./scripts/deploy/deploy_key_ds
+
 ssh-add scripts/deploy/deploy_key_ds
 
 
