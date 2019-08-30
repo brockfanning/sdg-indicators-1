237f513f0889bcb7e8b4674835bd45ea7ad6869d
MoBosse <moritz.bosse@destatis.de>
2019-08-30 09:23:20 +0200

    test toFixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,13 @@
 //
-//opensdg.dataRounding = function(value) {
-//  if (value == null) {
-//    return value
-//  }
-//  else {
-//    return value.toPrecision(3)
-//  }
-//};
-
 opensdg.dataRounding = function(value) {
-  return Math.round(value * 100) / 100;
+  if (value == null) {
+    return value
+  }
+  else {
+    //return value.toPrecision(3)
+    return value.toFixed(2)
+  }
+};
+
+//opensdg.dataRounding = function(value) {
+//  return Math.round(value * 100) / 100;
