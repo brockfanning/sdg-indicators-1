60265b1aa5178bc379b132ed1931a3ad55fc8b77
MoBosse <moritz.bosse@destatis.de>
2019-08-30 11:58:44 +0200

    undo custom rounding

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,10 +4,12 @@ opensdg.dataRounding = function(value) {
     return value
   }
   else {
+    //5 555 --> 5 560; 34,56 --> 34,6; 3,4 --> 3,40; 1 --> 1,00
     //return value.toPrecision(3)
-    return value.toFixed(2)
+
+    ////5 555 --> 5 555,00; 34,56 --> 34,65; 3,4 --> 3,40; 1 --> 1,00
+    //return value.toFixed(2)
+
+    return value
   }
 };
-
-//opensdg.dataRounding = function(value) {
-//  return Math.round(value * 100) / 100;
