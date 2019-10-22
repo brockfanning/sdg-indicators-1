98a4a2e252d59fbea20416174abc2b53c6b0b197
nachhaltige-entwicklung-deutschland <dns-indikatoren@destatis.de>
2019-10-22 12:06:46 +0200

    map colors 3.1

--- _includes/assets/js/plugins/jquery.sdgMaps.js
+++ _includes/assets/js/plugins/jquery.sdgMaps.js
@@ -77,7 +77,7 @@
     this.valueRange = [_.min(_.pluck(this.geoData, 'Value')), _.max(_.pluck(this.geoData, 'Value'))];
     this.colorScale = chroma.scale(this.options.colorRange)//[this.goalNr])
       .domain(this.valueRange)
-      .classes(10); //this.options.colorRange[3].length); //[this.goalNr].length);
+      .classes(this.options.colorRange.length); //[this.goalNr].length);
 
     this.years = _.uniq(_.pluck(this.geoData, 'Year')).sort();
     this.currentYear = this.years[0];
