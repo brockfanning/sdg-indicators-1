563f1de7aefcb9ddeff46fd44b03028b651d8d45
MoBosse <moritz.bosse@destatis.de>
2020-01-28 09:09:36 +0100

    update maps

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -270,13 +270,13 @@
       }));
 
       //---#7 addMapboxWordmark---start-----------------------------------------------------------------------------------------
-      var logo = L.control({position: 'bottomleft'});
-      logo.onAdd = function (map) {
-        var div = L.DomUtil.create('div', 'logo');
-        div.innerHTML = '<a href="https://mapbox.com"> <img src="https://g205sdgs.github.io/sdg-indicators/public/mapbox-logo-white.png"/ width=140 height=30> </a>'
-        return div;
-      };
-      logo.addTo(this.map);
+      //var logo = L.control({position: 'bottomleft'});
+      //logo.onAdd = function (map) {
+        //var div = L.DomUtil.create('div', 'logo');
+        //div.innerHTML = '<a href="https://mapbox.com"> <img src="https://g205sdgs.github.io/sdg-indicators/public/mapbox-logo-white.png"/ width=140 height=30> </a>'
+        //return div;
+      //};
+      //logo.addTo(this.map);
       //---#7 addMapboxWordmark---stop-----------------------------------------------------------------------------------------
 
       // Add the selection legend.
