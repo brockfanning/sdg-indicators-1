a6818d6293ff03be14cfc073a9f45f62b7c88055
MoBosse <moritz.bosse@destatis.de>
2020-02-26 14:52:49 +0100

    3.2

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -407,7 +407,7 @@ var indicatorView = function (model, options) {
             var text = ['<ul id="legend">'];
 
             _.each(chart.data.datasets, function(dataset, datasetIndex) {
-              if (dataset.label != ("treatment demand in facilities for addiction support (substances opioids, cocaine and stimulants) of the age group 18 to under 65 years(lower bound)")){
+              if (dataset.label != ("treatment demand in facilities for addiction support (substances opioids, cocaine and stimulants) of the age group 18 to under 65 years, lower bound")){
                 text.push('<li data-datasetindex="' + datasetIndex + '">');
                 text.push('<span class="swatch' + (dataset.borderDash ? ' dashed' : '') + '" style="background-color: ' + dataset.backgroundColor + '">');
                 text.push('</span>');
