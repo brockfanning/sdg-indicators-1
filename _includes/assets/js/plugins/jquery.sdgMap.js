14283d2f0c4b2272de7b1d3250e288945ec36d8c
MoBosse <moritz.bosse@destatis.de>
2020-07-01 14:13:24 +0200

    log element

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -75,7 +75,7 @@
 
     this._defaults = defaults;
     this._name = 'sdgMap';
-    console.log("a: options", this.options);
+    console.log("a: element", this.element);
     //---#2 TimeSeriesNameDisplayedInMaps---start--------------------------------------------------------------
     this.geoData = options.geoData;
     this.geoCodeRegEx = options.geoCodeRegEx;
@@ -83,7 +83,7 @@
     this.timeSeriesName = translations.t(this.timeSeries[this.timeSeries.length -1]);
     this.unit = _.pluck(this.geoData, 'Units');
     this.unitName = translations.t(this.unit[this.unit.length -1]);
-    console.log("b: timeseries", this.timeSeries);
+    console.log("b: timeseries", this.timeSeriesName);
     //---#2 TimeSeriesNameDisplayedInMaps---stop---------------------------------------------------------------
     this.init();
   }
