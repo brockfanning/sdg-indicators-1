0abc634654a9823a69b400888249642bdd8e9c69
MoBosse <moritz.bosse@destatis.de>
2020-07-02 12:16:12 +0200

    map titles 5.3

--- _includes/assets/js/mapTitles.js
+++ _includes/assets/js/mapTitles.js
@@ -1,8 +1,8 @@
 opensdg.maptitles = function(indicatorId) {
   if(indicatorId == "indicator_8-5-1"){
 
-    this.mapTitle = "Test2345"
-    this.mapUnit = "TestUnit"
+    this.mapTitle = translations.t("gender pay gap")
+    this.mapUnit = translations.t("%")
   }
 
   return [this.mapTitle, this.mapUnit] ;
