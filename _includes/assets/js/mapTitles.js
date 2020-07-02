3d749bfa3ba9631183c1b0cb5ef61316df448bad
MoBosse <moritz.bosse@destatis.de>
2020-07-02 12:29:48 +0200

    map titles 5.4

--- _includes/assets/js/mapTitles.js
+++ _includes/assets/js/mapTitles.js
@@ -1,7 +1,17 @@
 opensdg.maptitles = function(indicatorId) {
-  if(indicatorId == "indicator_8-5-1"){
+  if(indicatorId == "indicator_5-5-1"){
+
+    this.mapTitle = translations.t("seats held by women in national parliament")
+    this.mapUnit = translations.t("%")
+  }
+  else if(indicatorId == "indicator_8-5-1"){
 
     this.mapTitle = translations.t("gender pay gap")
+    this.mapUnit = translations.t("%")
+  }
+  else if(indicatorId == "indicator_9-5-1"){
+
+    this.mapTitle = translations.t("roportion of r&d expenditures to gdp")
     this.mapUnit = translations.t("%")
   }
 
