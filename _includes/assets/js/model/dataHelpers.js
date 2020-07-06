c69c0668ca267c905db983e769fa6296185e328c
MoBosse <moritz.bosse@destatis.de>
2020-07-06 15:53:30 +0200

    data rounding 3.0

--- _includes/assets/js/model/dataHelpers.js
+++ _includes/assets/js/model/dataHelpers.js
@@ -48,7 +48,7 @@ function prepareData(rows) {
       // object, for easier control: opensdg.dataRounding()
       if (typeof opensdg.dataRounding === 'function') {
         if (typeof opensdg.dataRoundingDp === 'function' && item.dcmplc != null){
-          console.log("test data Rounding function")
+          item.Value = opensdg.dataRoundingDp(item.Value, item.dcmplc);
         }
         else{
           item.Value = opensdg.dataRounding(item.Value);
